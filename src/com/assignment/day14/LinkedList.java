713e4026705328546c23f846b8904aeff09cfb9b
Subhashree naik <subhashree.naik@gmail.com>
2022-03-03 20:17:22 +0530

    SortingLinkedList

--- src/com/assignment/day14/LinkedList.java
+++ src/com/assignment/day14/LinkedList.java
@@ -102,6 +102,34 @@ public class LinkedList {
 	        prev.next = temp.next;
 	    }
 	 
+	 public void sortList() {  
+	          
+	        Node current = head, index = null;  
+	        int temp;  
+	          
+	        if(head == null) {  
+	            return;  
+	        }  
+	        else {  
+	            while(current != null) {  
+	                
+	                index = current.next;  
+	                  
+	                while(index != null) {  
+	                    //If current node's data is greater than index's node data, swap the data between them  
+	                    if(current.data > index.data) {  
+	                        temp = current.data;  
+	                        current.data = index.data;  
+	                        index.data = temp;  
+	                    }  
+	                    index = index.next;  
+	                }  
+	                current = current.next;  
+	            }      
+	        }  
+	    }  
+	      
+	 
 	    
 	 
 }
